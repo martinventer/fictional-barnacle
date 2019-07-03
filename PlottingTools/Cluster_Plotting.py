0c26f190a362653d65dfb9a5f39a67148b7485bc
martinventer <mpventer@sun.ac.za>
2019-07-03 10:42:04 +0200

    Scatter plots for clustering added to PlottingTools

--- PlottingTools/Cluster_Plotting.py
+++ PlottingTools/Cluster_Plotting.py
@@ -10,12 +10,15 @@ Tools for plotting clusters
 
 import numpy as np
 from matplotlib import pyplot as plt
+import seaborn as sns
 from scipy.cluster.hierarchy import dendrogram
+
+from sklearn.decomposition import PCA
 
 from sklearn.pipeline import Pipeline
 
 
-def plot_dendrogram(children, **kwargs):
+def plot_dendrogram(children, **kwargs) -> None:
     # determine distance between each pair
     distance = position = np.arange(children.shape[0])
 
@@ -33,6 +36,12 @@ def plot_dendrogram(children, **kwargs):
     plt.show()
 
 
+def plot_clusters(X, y, **kwargs) -> None:
+
+    fig, ax = plt.subplots(figsize=(10, 5))
+    ax = sns.scatterplot(x=X[:,0], y=X[:,1], hue=y)
+    plt.tight_layout()
+    plt.show()
 
 
 if __name__ == '__main__':
@@ -47,7 +56,6 @@ if __name__ == '__main__':
     subset = next(loader.fileids(test=True))
 
     docs = list(corpus.title_tagged(fileids=subset))
-    pickles = subset
 
     # # Plot hierarchical clustering
     # model = Pipeline([
@@ -61,3 +69,25 @@ if __name__ == '__main__':
     # children = model.named_steps['clusters'].children
     #
     # plot_dendrogram(children)
+
+    # decompose data to 2D
+    reduce = Pipeline([
+        ("norm", Corpus_Vectorizer.TitleNormalizer()),
+        ("vect", Corpus_Vectorizer.OneHotVectorizer()),
+        ('pca', PCA(n_components=2))
+    ])
+
+    X2d = reduce.fit_transform(docs)
+
+    # plot Kmeans
+    model = Pipeline([
+        ("norm", Corpus_Vectorizer.TitleNormalizer()),
+        ("vect", Corpus_Vectorizer.OneHotVectorizer()),
+        ('clusters', Corpus_Cluster.MiniBatchKMeansClusters(k=3))
+    ])
+
+    clusters = model.fit_transform(docs)
+
+    plot_clusters(X2d, clusters)
+
+
