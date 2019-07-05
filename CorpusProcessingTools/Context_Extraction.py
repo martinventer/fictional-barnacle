3966fbb449c7fcbbf57d3408b041db604fe71186
martinventer <mpventer@sun.ac.za>
2019-07-05 15:49:44 +0200

    Added added tests for significant collocation of grams2

--- CorpusProcessingTools/Context_Extraction.py
+++ CorpusProcessingTools/Context_Extraction.py
@@ -161,6 +161,31 @@ def rank_quadgrams(docs, metric, path=None):
         return scored
 
 
+class RankGrams(BaseEstimator, TransformerMixin):
+
+    def __init__(self):
+
+def rank_grams(docs, metric, path=None):
+    """
+    Find and rank quadgrams from the supplied corpus using the given
+    association metric. Write the quadgrams out to the given path if
+    supplied otherwise return the list in memory.
+    """
+
+    # Create a collocation ranking utility from corpus words.
+    ngrams = QuadgramCollocationFinder.from_words(docs)
+
+    # Rank collocations by an association metric
+    scored = ngrams.score_ngrams(metric)
+
+    if path:
+        with open(path, 'w') as f:
+            f.write("Collocation\tScore ({})\n".format(metric.__name__))
+            for ngram, score in scored:
+                f.write("{}\t{}\n".format(repr(ngram), score))
+    else:
+        return scored
+
 if __name__ == '__main__':
     from CorpusReader import Elsevier_Corpus_Reader
     from CorpusProcessingTools import Corpus_Vectorizer
@@ -199,3 +224,6 @@ if __name__ == '__main__':
     for gram, score in dat:
         if "soft" in gram and score > 2500:
             print(gram, score)
+
+    for i in range(20):
+        print(dat[i])
