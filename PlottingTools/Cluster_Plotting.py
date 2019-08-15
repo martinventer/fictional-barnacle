703e95a93fa0870ace2d0c99fcb193e6960ce545
martinventer <mpventer@sun.ac.za>
2019-08-15 08:09:19 +0200

    Cluster_Plotting depricated content converted to class objects and moved to TextTools.Analysis

--- PlottingTools/Cluster_Plotting.py
+++ PlottingTools/Cluster_Plotting.py
@@ -1,67 +1,66 @@
-#! envs/fictional-barnacle/bin/python3.6
-"""
-Cluster_Plotting.py
-
-@author: martinventer
-@date: 2019-07-03
-
-Tools for plotting clusters
-"""
-
-from sklearn.decomposition import PCA
-
-from sklearn.pipeline import Pipeline
-
-from TextTools.Analysis import plot_clusters
-
-if __name__ == '__main__':
-    from CorpusReaders import Elsevier_Corpus_Reader
-    from Depricated import Corpus_Cluster, Corpus_Vectorizer
-
-    corpus = Elsevier_Corpus_Reader.ScopusProcessedCorpusReader(
-        "Corpus/Processed_corpus/")
-
-    loader = Elsevier_Corpus_Reader.CorpuKfoldLoader(corpus, 100, shuffle=False)
-    subset = next(loader.fileids(test=True))
-
-    docs = list(corpus.title_tagged(fileids=subset))
-
-    # --------------------------------------------------------------------------
-    # plot_dendrogram
-    # --------------------------------------------------------------------------
-    # model = Pipeline([
-    #     ("norm", Corpus_Vectorizer.TitleNormalizer()),
-    #     ("vect", Corpus_Vectorizer.OneHotVectorizer()),
-    #     ('clusters', Corpus_Cluster.HierarchicalClustering())
-    # ])
-    #
-    # clusters = model.fit_transform(observations)
-    # labels = model.named_steps['clusters'].labels
-    # children = model.named_steps['clusters'].children
-    #
-    # plot_dendrogram(children)
-
-    # --------------------------------------------------------------------------
-    # plot_clusters
-    # --------------------------------------------------------------------------
-    # decompose data to 2D
-    reduce = Pipeline([
-        ("norm", Corpus_Vectorizer.TitleNormalizer()),
-        ("vect", Corpus_Vectorizer.OneHotVectorizer()),
-        ('pca', PCA(n_components=2))
-    ])
-
-    X2d = reduce.fit_transform(docs)
-
-    # plot Kmeans
-    model = Pipeline([
-        ("norm", Corpus_Vectorizer.TitleNormalizer()),
-        ("vect", Corpus_Vectorizer.OneHotVectorizer()),
-        ('clusters', Corpus_Cluster.MiniBatchKMeansClusters(k=3))
-    ])
-
-    clusters = model.fit_transform(docs)
-
-    plot_clusters(X2d, clusters)
-
-
+# #! envs/fictional-barnacle/bin/python3.6
+# """
+# Cluster_Plotting.py
+#
+# @author: martinventer
+# @date: 2019-07-03
+#
+# Tools for plotting clusters
+# """
+#
+#
+# from sklearn.pipeline import Pipeline
+#
+# from TextTools.Analysis import plot_clusters
+#
+# if __name__ == '__main__':
+#     from CorpusReaders import Elsevier_Corpus_Reader
+#     from Depricated import Corpus_Cluster, Corpus_Vectorizer
+#
+#     corpus = Elsevier_Corpus_Reader.ScopusProcessedCorpusReader(
+#         "Corpus/Processed_corpus/")
+#
+#     loader = Elsevier_Corpus_Reader.CorpuKfoldLoader(corpus, 100, shuffle=False)
+#     subset = next(loader.fileids(test=True))
+#
+#     docs = list(corpus.title_tagged(fileids=subset))
+#
+#     # --------------------------------------------------------------------------
+#     # plot_dendrogram
+#     # --------------------------------------------------------------------------
+#     # model = Pipeline([
+#     #     ("norm", Corpus_Vectorizer.TitleNormalizer()),
+#     #     ("vect", Corpus_Vectorizer.OneHotVectorizer()),
+#     #     ('clusters', Corpus_Cluster.HierarchicalClustering())
+#     # ])
+#     #
+#     # clusters = model.fit_transform(observations)
+#     # labels = model.named_steps['clusters'].labels
+#     # children = model.named_steps['clusters'].children
+#     #
+#     # plot_dendrogram(children)
+#
+#     # --------------------------------------------------------------------------
+#     # plot_clusters
+#     # --------------------------------------------------------------------------
+#     # # decompose data to 2D
+#     # reduce = Pipeline([
+#     #     ("norm", Corpus_Vectorizer.TitleNormalizer()),
+#     #     ("vect", Corpus_Vectorizer.OneHotVectorizer()),
+#     #     ('pca', PCA(n_components=2))
+#     # ])
+#     #
+#     # X2d = reduce.fit_transform(docs)
+#     #
+#     # # plot Kmeans
+#     # model = Pipeline([
+#     #     ("norm", Corpus_Vectorizer.TitleNormalizer()),
+#     #     ("vect", Corpus_Vectorizer.OneHotVectorizer()),
+#     #     ('clusters', Corpus_Cluster.MiniBatchKMeansClusters(k=3))
+#     # ])
+#     #
+#     # clusters = model.fit_transform(docs)
+#     #
+#     # plot_clusters(X2d, clusters)
+#
+#
--- TextTools/Analysis.py
+++ TextTools/Analysis.py
@@ -12,8 +12,13 @@ import seaborn as sns
 from matplotlib import pyplot as plt
 from scipy import sparse
 from scipy.cluster.hierarchy import dendrogram
-from sklearn.manifold import TSNE
+
 from sklearn.base import BaseEstimator, TransformerMixin
+
+from sklearn.manifold import TSNE, SpectralEmbedding
+
+from sklearn.decomposition import TruncatedSVD, PCA, \
+    LatentDirichletAllocation, NMF
 
 
 class DendrogramPlot:
@@ -59,65 +64,92 @@ class DendrogramPlot:
         plt.show()
 
 
+class ClusterPlot2D:
+    """
+    plotting class for cluster results
+    """
+    methods = {"SVD" : TruncatedSVD,
+               "PCA" : PCA,
+               "LDA" : LatentDirichletAllocation,
+               "NMF" : NMF,
+               "SE" : SpectralEmbedding,
+               "TSNE" : TSNE}
 
+    def __init__(self, clusters, labels, method="TSNE"):
+        self.clusters = clusters
+        self.labels = labels
+        self.decompose = ClusterPlot2D.methods[method](n_components=2)
+        self.data_2d = None
+        self.data_process()
 
-def plot_dendrogram(children, **kwargs) -> None:
-    # determine distance between each pair
-    distance = position = np.arange(children.shape[0])
+    def data_process(self) -> None:
+        """
+        converts sparse matrix to dense 2D projection
+        Returns
+        -------
+            None
+        """
+        if type(self.clusters) is sparse.csr.csr_matrix:
+            self.clusters = self.clusters.toarray()
+        X2d = self.decompose.fit_transform(self.clusters)
+        x_min, x_max = np.min(X2d, axis=0), np.max(X2d, axis=0)
+        self.data_2d = (X2d - x_min) / (x_max - x_min)
 
-    # create linkage matrix
-    linkage_matrix = np.column_stack([
-        children, distance, position
-    ]).astype(float)
+    def plot(self, **kwargs) -> None:
+        """
+        Plots a 2D scatter plot of the clusterd data with coloured text to
+        labels for each data point.
+        Parameters
+        ----------
+        kwargs
+            additional plotting arguements
 
-    # plot dendrogram
-    fig, ax = plt.subplots(figsize=(10, 5))
-    ax = dendrogram(linkage_matrix, **kwargs)
-    plt.tick_params(axis='x', bottom='off', top='off', labelbottom="off")
-    plt.tight_layout()
-    plt.show()
+        Returns
+        -------
+            None
+        """
+        plt.figure(figsize=(15, 9))
+        for i in range(self.data_2d.shape[0]):
+            plt.text(self.data_2d[i, 0],
+                     self.data_2d[i, 1],
+                     str(self.labels[i]),
+                     color=plt.cm.nipy_spectral(self.labels[i] / 10.),
+                     fontdict={'weight': 'bold', 'size': 9})
 
+        plt.xticks([])
+        plt.yticks([])
+        plt.axis('off')
+        plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+        plt.show()
 
-def plot_clusters(X, y, **kwargs) -> None:
+    def simple_plot(self, **kwargs) -> None:
+        """
+        Plots a 2D scatter plot of the clusterd data with coloured points to
+        labels for each data point.
+        Parameters
+        ----------
+        kwargs :
+            Additional plotting arguements
 
-    fig, ax = plt.subplots(figsize=(10, 5))
-    ax = sns.scatterplot(x=X[:,0], y=X[:,1], hue=y)
-    plt.tight_layout()
-    plt.show()
+        Returns
+        -------
 
-
-def plot_clusters_2d(X, y, **kwargs) -> None:
-    if type(X) is sparse.csr.csr_matrix:
-        X = X.toarray()
-    # reduce = TruncatedSVD(n_components=2)
-    # reduce = PCA(n_components=2)
-    # reduce = LatentDirichletAllocation(n_components=2)
-    # reduce = NMF(n_components=2)
-    # reduce = SpectralEmbedding(n_components=2)
-    reduce = TSNE(n_components=2)
-
-    X2d = reduce.fit_transform(X)
-
-    x_min, x_max = np.min(X2d, axis=0), np.max(X2d, axis=0)
-    X_red = (X2d - x_min) / (x_max - x_min)
-
-    plt.figure(figsize=(15, 9))
-    for i in range(X_red.shape[0]):
-        plt.text(X_red[i, 0], X_red[i, 1], str(y[i]),
-                 color=plt.cm.nipy_spectral(y[i] / 10.),
-                 fontdict={'weight': 'bold', 'size': 9})
-
-    plt.xticks([])
-    plt.yticks([])
-    plt.axis('off')
-    plt.tight_layout(rect=[0, 0.03, 1, 0.95])
-    plt.show()
+        """
+        fig, ax = plt.subplots(figsize=(10, 5))
+        ax = sns.scatterplot(
+            x=self.data_2d[:, 0],
+            y=self.data_2d[:, 1],
+            hue=self.labels,
+            **kwargs)
+        plt.tight_layout()
+        plt.show()
 
 
 if __name__ == '__main__':
     from CorpusReaders import Elsevier_Corpus_Reader
     from TextTools import Transformers
     from sklearn.pipeline import Pipeline
+    from sklearn.decomposition import TruncatedSVD
 
     root = "Tests/Test_Corpus/Processed_corpus/"
     corpus = Elsevier_Corpus_Reader.ScopusProcessedCorpusReader(
@@ -130,15 +162,58 @@ if __name__ == '__main__':
     # ==========================================================================
     # DendrogramPlot
     # ==========================================================================
-    model = Pipeline([
-        ("norm", Transformers.TextNormalizer()),
-        ("vect", Transformers.Text2OneHotVector()),
-        ('clusters', Transformers.HierarchicalClustering())
-    ])
+    if False:
+        model = Pipeline([
+            ("norm", Transformers.TextNormalizer()),
+            ("vect", Transformers.Text2OneHotVector()),
+            ('clusters', Transformers.HierarchicalClustering())
+        ])
 
-    clusters = model.fit_transform(observations)
-    children = model.named_steps['clusters'].children
+        clusters = model.fit_transform(observations)
+        children = model.named_steps['clusters'].children
 
+        tree_plotter = DendrogramPlot(children)
+        tree_plotter.plot()
 
-    tree_plotter = DendrogramPlot(children)
-    tree_plotter.plot()
+    # ==========================================================================
+    # plot_clusters
+    # ==========================================================================
+    if False:
+        # decompose data to 2D
+        reduce = Pipeline([
+            ("norm", Transformers.TextNormalizer()),
+            ("vect", Transformers.Text2OneHotVector()),
+            ('pca', TruncatedSVD(n_components=2))
+        ])
+
+        X2d = reduce.fit_transform(observations)
+
+        # plot Kmeans
+        model = Pipeline([
+            ("norm", Transformers.TextNormalizer()),
+            ("vect", Transformers.Text2OneHotVector()),
+            ('clusters', Transformers.KMeansClusters(k=3))
+        ])
+
+        clusters = model.fit_transform(observations)
+
+        plot_clusters(X2d, clusters)
+        # plot_clusters_2d
+
+    # ==========================================================================
+    # plot_clusters
+    # ==========================================================================
+    if False:
+        prepare_data = Pipeline(
+            [('normalize', Transformers.TextNormalizer()),
+             ('vectorize', Transformers.Text2OneHotVector()
+              )])
+
+        cluster_data = Transformers.KMeansClusters(k=5)
+
+        data = prepare_data.fit_transform(observations)
+        labels = cluster_data.fit_transform((data))
+
+        cluster_plotter = ClusterPlot2D(data, labels, method="NMF")
+        cluster_plotter.plot()
+        cluster_plotter.simple_plot()
